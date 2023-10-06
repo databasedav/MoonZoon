d959b629f3461eb10be0c8ae6cc9db676c445189
avi <senavi97@gmail.com>
2023-10-06 02:11:03 -0700

    fix bounds

--- examples/svg/frontend/src/main.rs
+++ examples/svg/frontend/src/main.rs
@@ -6,7 +6,7 @@ use zoon::{
     println,
 };
 use rust_hsluv;
-use rand::{Rng, thread_rng};
+use rand::{Rng, rngs::SmallRng, SeedableRng, seq::{SliceRandom, IteratorRandom}, distributions::{Alphanumeric, DistString}, thread_rng};
 
 // ------ ------
 //    Types
@@ -63,15 +63,15 @@ fn next_light_state() {
 //     View
 // ------ ------
 
-fn triangle_height(side: f32) -> f32 {
-    3.0_f32.sqrt() / 2.0 * side
+fn triangle_height(side: f64) -> f64 {
+    3.0_f64.sqrt() / 2.0 * side
 }
 
-fn triangle_side(height: f32) -> f32 {
-    2.0 * height / 3.0_f32.sqrt()
+fn triangle_side(height: f64) -> f64 {
+    2.0 * height / 3.0_f64.sqrt()
 }
 
-fn triangle(side: f32, color: HSLuv) -> impl Element + Styleable<'static> {
+fn triangle(side: f64, color: HSLuv) -> impl Element + Styleable<'static> {
     let height = triangle_height(side).to_string();
     El::new()
     // .s(Height::growable())
@@ -113,13 +113,41 @@ fn hsluv_from_hex(hex: &str) -> HSLuv {
     HSLuv::hsl(h, s, l)
 }
 
+fn get_rng() -> SmallRng {
+    SmallRng::from_rng(thread_rng()).expect_throw("failed to get rng")
+}
+
+fn random_range<T: rand::distributions::uniform::SampleUniform>(range: impl rand::distributions::uniform::SampleRange<T>) -> T {
+    get_rng().gen_range(range)
+}
+
+const LOGO_MAX_BREATHING_SCALE: f64 = 3.5;
+const LOGO_TRIANGLE_SIDE: f64 = 40.;
+const LOGO_BEAT_SCALE: f64 = 5.;
+
 fn breathify(el: RawSvgEl<web_sys::SvgElement>) -> RawSvgEl<web_sys::SvgElement> {
-    let oscillator = Oscillator::new(Duration::milliseconds(500));
+    let min_duration: i64 = 200;
+    let max_duration = 500;
+    let mut duration = random_range(min_duration..max_duration);
+    let oscillator = Oscillator::new(Duration::milliseconds(duration));
     oscillator.jump_to(js_sys::Math::random());
+    let duration_randomizer = Task::start_droppable(clone!((oscillator) async move {
+        loop {
+            Timer::sleep(duration as u32).await;
+            duration = random_range(min_duration..max_duration);
+            oscillator.set_duration(zoon::Duration::milliseconds(duration));
+        }
+    }));
+    let side = 40.;
+    let height = triangle_height(side);
+    let beat_scale = 1.05;
     oscillator.cycle();
     RawSvgEl::new("g")
-    .attr("transform-origin", "center")
-    .attr_signal("transform", oscillator.signal().map(|oscillation| format!("scale({})", 1. + (oscillation * 0.035))))
+    .attr("width", "100%")
+    .attr("height", "100%")
+    .after_remove(move |_| drop(duration_randomizer))
+    .attr("transform-origin", "center center")
+    .attr_signal("transform", oscillator.signal().map(|oscillation| format!("scale({})", 1. + (oscillation * LOGO_MAX_BREATHING_SCALE / 100.))))
     .child(el)
 }
 
@@ -144,27 +172,35 @@ fn beatify(el: RawSvgEl<web_sys::SvgElement>) -> RawSvgEl<web_sys::SvgElement> {
         })
         .await;
     }));
+    let side = 40.;
+    let height = triangle_height(side);
+    let beat_scale = 1.05;
     RawSvgEl::new("g")
-    .attr("height", "150")
-    .attr("width", "100")
+    .attr("width", "100%")
+    .attr("height", "100%")
     .after_remove(move |_| drop(task))
-    .attr_signal("transform", scale.signal().map_true(|| format!("scale(1.05)")))
+    .attr_signal("transform", scale.signal().map_true(|| format!("scale({})", 1. + LOGO_BEAT_SCALE / 100.)))
     .attr("transform-origin", "center")
     .child(el)
 }
 
 fn root() -> impl Element {
-    let side = 40.;
+    let side: f64 = 40.;
     let height = triangle_height(side);
+    let beat_scale = 1.05;
 
     El::new()
     .s(Align::center())
     .child(
         RawSvgEl::new("svg")
-        .attr("height", "150")
-        .attr("width", "100")
+        .attr("width", &(height * 2.0 * beat_scale * (1. + LOGO_MAX_BREATHING_SCALE / 100.)).to_string())
+        .attr("height", &(100. * beat_scale * (1. + LOGO_MAX_BREATHING_SCALE / 100.)).to_string())
+        // .attr("viewBox", &format!("0 0 {} {}", height * 2.0 * beat_scale, 100. * beat_scale))
         .child(
             RawSvgEl::new("g")
+            .attr("transform", &format!("translate({}, {})", (height * 2.0 * beat_scale * (1. + LOGO_MAX_BREATHING_SCALE / 100.) - (height * 2.0)) * 0.5, (100. * beat_scale * (1. + LOGO_MAX_BREATHING_SCALE / 100.) - 100.) * 0.5))
+            .attr("width", "100%")
+            .attr("height", "100%")
             .child(
                 RawSvgEl::new("polygon")
                 .attr("points", &format!("0,0, {},0, {},{}", height * 2.0, height, side / 2.0))
@@ -180,31 +216,37 @@ fn root() -> impl Element {
             .child(
                 RawSvgEl::new("g")
                 .attr("transform", &format!("translate({})", height))
+                .attr("fill", "#cccccc")
                 .child(
-                    RawSvgEl::new("g")
-                    .attr("fill", "#cccccc")
-                    .child(
-                        RawSvgEl::new("polygon")
-                        .attr("id", "triangle")
-                        .attr("transform", &format!("rotate(-90 {} {})", side / 2.0, side / 2.0))
-                        .attr("points", &format!("{} 0, 0 {}, {} {}", side / 2.0, &height, side, &height))
-                    )
-                    .apply(breathify)
+                    RawSvgEl::new("polygon")
+                    .attr("id", "triangle")
+                    .attr("transform", &format!("rotate(-90 {} {})", side / 2.0, side / 2.0))
+                    .attr("points", &format!("{} 0, 0 {}, {} {}", side / 2.0, &height, side, &height))
                 )
+                .apply(breathify)
+            )
+            .child(
+                RawSvgEl::new("g")
+                .attr("transform", &format!("translate({})", height))
                 .child(
                     RawSvgEl::new("use")
                     .attr("href", "#triangle")
                     .attr("transform", &format!("rotate(180 {} {}) translate(0, -20)", height / 2.0, side / 2.0))
                     .attr("fill", "#999999")
-                    .apply(breathify)
                 )
+                .apply(breathify)
+            )
+            .child(
+                RawSvgEl::new("g")
+                .attr("transform", &format!("translate({})", height))
                 .child(
                     RawSvgEl::new("polygon")
+                    .attr("transform", &format!("translate({})", height))
                     .attr("transform", "translate(0, 60)")
                     .attr("points", &format!("0,0, {},-20, {},40, 0,40", height, height))
                     .attr("fill", "#666666")
-                    .apply(breathify)
                 )
+                .apply(breathify)
             )
             .apply(beatify)
         )
